ed2884b11c01b035de31f5fe163e27684f87dc37
unknown <osmanliazer@gmail.com>
2024-03-19 01:12:41 +0400

    task

---
+++ homework methods.cs
@@ -0,0 +1,23 @@
+﻿namespace metodsCsarph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 4;
+            double b = 5;
+            double are = FindRectangle(4, 5);
+            Console.Writeline(are);
+
+            {
+                static double FindRectangle(double a, double b)
+                {
+                    return a * b;
+                }
+            }
+
+
+
+
+        }
+    }
---
+++ methods.cs
@@ -0,0 +1,39 @@
+﻿//namespace homework_methods2
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            double r = 3;
+//            double area = FindCircle(r);
+//            Console.WriteLine(area);
+//        }
+//        static double FindCircle(double r)
+//        {
+//            double p = 3;
+//            return p * r * r;
+//        }
+//    }
+//}
+
+
+
+//namespace metodsCsarph
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            double a = 4;
+//            double b = 5;
+//            double are = FindRectangle(4, 5);
+//            Console.Writeline(are);
+
+//            {
+//                static double FindRectangle(double a, double b)
+//                {
+//                    return(a * b);
+//                }
+//            }
+//        }
+//    }
